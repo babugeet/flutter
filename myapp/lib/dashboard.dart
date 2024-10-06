d6ce34775d131a309aa5e2e452d576c1e0721049
babugeet <abhinandhbg@gmail.com>
2024-10-07 02:03:15 +0530

    fixed dashboard, profile

--- myapp/lib/dashboard.dart
+++ myapp/lib/dashboard.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:myapp/profilescreen.dart';
 import 'package:myapp/workoutscreen.dart';
-import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -22,19 +21,6 @@ class _DashboardPageState extends State<DashboardPage> {
   String _age = '';
   String _weight = '';
 
-  // List of widgets for each section
-  final List<Widget> _widgetOptions = <Widget>[
-    DashboardScreen(),
-    WorkoutsScreen(),
-    ProfileScreen(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
   @override
   void initState() {
     super.initState();
@@ -42,67 +28,62 @@ class _DashboardPageState extends State<DashboardPage> {
   }
 
   Future<void> _fetchUserProfile() async {
-    // Retrieve the token and username from SharedPreferences
     final prefs = await SharedPreferences.getInstance();
     String? token = prefs.getString('token1');
     String? username = prefs.getString('userna1me');
-print("token not found");
-print(token);
-print(username);
+    
     if (token != null && username != null) {
-      print("token found");
-      // Prepare the authorization header
-      String authHeader = 'Bearer ${token}';
-      print(authHeader);
-      print(username);
+      String authHeader = 'Bearer $token';
       try {
-        // Make the GET request
-        // Response response = await Dio().get(
-        //   'http://localhost:8080/getuser/$username',
-        //   options: Options(headers: {'Authorization': authHeader}),
+        final response = await http.post(
+        //   Uri.parse('http://localhost:8080/getuser/$username'),
+        //   headers: <String, String>{
+        //     'Content-Type': 'application/json; charset=UTF-8',
+        //     'Authorization': authHeader,
+        //   },
         // );
-      // final response = await http.post(
-      //   Uri.parse('http://localhost:8080/getuser/$username'),
-      //   headers: <String, String>{
-      //     'Content-Type': 'application/json; charset=UTF-8',
-      //     // 'Accept': 'application/json',
-      //     'Origin':'http://localhost:8081',
-      //    'Authorization': authHeader,
-      //    'Accept': '*/*',
-      //   },
-      // );
-      Response response = await Dio().post(
-  'http://localhost:8080/getuser/$username',
-  options: Options(
-    headers: {
-      'Authorization': 'Bearer $token',
-      'Content-Type': 'application/json',
-      'Origin': 'http://localhost:8080',
-    },
-  ),  data: { 
-    // Include any data you want to send in the body, if needed
-    'someKey': 'someValue' 
-  },
-);
-        // Check the response status
+                // final response = await http.post(
+          Uri.parse('http://localhost:8080/getuser/$username'),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Origin': 'http://localhost:8081',
+            'Authorization': authHeader,
+            'Accept': '*/*',
+          },
+          body: jsonEncode(<String, String>{
+            'someKey': 'someValue', // If you need to send any data in the body
+          }),
+        );
+
         if (response.statusCode == 200) {
-          // Parse the response and update the state
-          // var userData = response.body;
-          // print(userData);
-          // setState(() {
-          //   _username = userData['username'] ?? '';
-          //   _age = userData['age']?.toString() ?? 'N/A';
-          //   _weight = userData['weight']?.toString() ?? 'N/A';
-          // });
+          String rawData = response.body;
+          if (rawData.startsWith('w')) {
+            rawData = rawData.substring(1); // Remove leading character
+          }
+          var userData = jsonDecode(rawData);
+
+          setState(() {
+            _username = userData['username'] ?? '';
+            _age = (userData['age'] != null && userData['age'] >= 0)
+                ? userData['age'].toString()
+                : 'N/A';
+            _weight = (userData['weight'] != null && userData['weight'] >= 0)
+                ? userData['weight'].toString()
+                : 'N/A';
+          });
         } else {
-          // Handle the error response
           print('Error fetching user profile: ${response.statusCode}');
         }
       } catch (e) {
-        // Handle any errors
         print('Error: $e');
       }
     }
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
   @override
@@ -117,7 +98,11 @@ print(username);
         elevation: 0,
         centerTitle: true,
       ),
-      body: _widgetOptions.elementAt(_selectedIndex), // Display selected page
+      body: _selectedIndex == 0 
+          ? DashboardScreen(username: _username, age: _age, weight: _weight) 
+          : _selectedIndex == 1 
+            ? WorkoutsScreen() 
+            : ProfileScreen(), // Assuming WorkoutsScreen and ProfileScreen are stateless too
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
@@ -147,7 +132,16 @@ print(username);
 
 // Main Dashboard Screen
 class DashboardScreen extends StatelessWidget {
-  const DashboardScreen({super.key});
+  final String username;
+  final String age;
+  final String weight;
+
+  const DashboardScreen({
+    Key? key,
+    required this.username,
+    required this.age,
+    required this.weight,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -156,7 +150,7 @@ class DashboardScreen extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          _buildUserProfile(context), // Pass context to fetch user profile
+          _buildUserProfile(),
           SizedBox(height: 20),
           _buildWorkoutsCompleted(),
           SizedBox(height: 20),
@@ -171,9 +165,7 @@ class DashboardScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildUserProfile(BuildContext context) {
-    final _DashboardPageState dashboardState = context.findAncestorStateOfType<_DashboardPageState>()!;
-
+  Widget _buildUserProfile() {
     return Card(
       elevation: 6,
       shape: RoundedRectangleBorder(
@@ -185,14 +177,14 @@ class DashboardScreen extends StatelessWidget {
           children: <Widget>[
             CircleAvatar(
               radius: 40,
-              backgroundImage: AssetImage('assets/user_profile.jpg'),
+              backgroundImage: AssetImage('user_profile.jpg'),
             ),
             SizedBox(width: 16),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 Text(
-                  dashboardState._username.isNotEmpty ? dashboardState._username : 'Loading...',
+                  username.isNotEmpty ? username : 'Loading...',
                   style: TextStyle(
                     fontSize: 24,
                     fontWeight: FontWeight.bold,
@@ -200,11 +192,11 @@ class DashboardScreen extends StatelessWidget {
                   ),
                 ),
                 Text(
-                  'Age: ${dashboardState._age.isNotEmpty ? dashboardState._age : 'Loading...'}',
+                  'Age: ${age.isNotEmpty ? age : 'Loading...'}',
                   style: TextStyle(color: Colors.grey.shade600),
                 ),
                 Text(
-                  'Weight: ${dashboardState._weight.isNotEmpty ? dashboardState._weight : 'Loading...'} kg',
+                  'Weight: ${weight.isNotEmpty ? weight : 'Loading...'} kg',
                   style: TextStyle(color: Colors.grey.shade600),
                 ),
               ],
@@ -314,21 +306,6 @@ class DashboardScreen extends StatelessWidget {
                         ),
                       ],
                     ),
-                    BarChartGroupData(
-                      x: 2,
-                      barRods: [
-                        BarChartRodData(
-                          toY: 25,
-                          color: Colors.redAccent,
-                          width: 20,
-                          backDrawRodData: BackgroundBarChartRodData(
-                            show: true,
-                            toY: 50,
-                            color: Colors.red.shade100,
-                          ),
-                        ),
-                      ],
-                    ),
                   ],
                 ),
               ),
@@ -352,10 +329,10 @@ class DashboardScreen extends StatelessWidget {
           children: <Widget>[
             Row(
               children: [
-                Icon(Icons.local_drink, color: Colors.lightBlueAccent),
+                Icon(Icons.local_drink, color: Colors.blue),
                 SizedBox(width: 10),
                 Text(
-                  'Daily Water Intake',
+                  'Water Intake',
                   style: TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
@@ -366,13 +343,16 @@ class DashboardScreen extends StatelessWidget {
             ),
             SizedBox(height: 10),
             LinearProgressIndicator(
-              value: 0.75, // 75% of water intake goal
+              value: 0.5,
+              minHeight: 10,
               backgroundColor: Colors.grey.shade300,
               color: Colors.blueAccent,
             ),
             SizedBox(height: 10),
-            Text('Goal: 3 L', style: TextStyle(color: Colors.grey.shade600)),
-            Text('Intake: 2.25 L', style: TextStyle(color: Colors.grey.shade600)),
+            Text(
+              'You have consumed 1.5 liters today.',
+              style: TextStyle(color: Colors.grey.shade600),
+            ),
           ],
         ),
       ),
@@ -392,10 +372,10 @@ class DashboardScreen extends StatelessWidget {
           children: <Widget>[
             Row(
               children: [
-                Icon(Icons.directions_walk, color: Colors.purpleAccent),
+                Icon(Icons.directions_walk, color: Colors.orange),
                 SizedBox(width: 10),
                 Text(
-                  'Steps Counter',
+                  'Steps Counted',
                   style: TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
@@ -405,7 +385,7 @@ class DashboardScreen extends StatelessWidget {
               ],
             ),
             SizedBox(height: 10),
-            Text('Steps Today: 7,500', style: TextStyle(color: Colors.grey.shade600)),
+            Text('You have walked 5,000 steps today.', style: TextStyle(color: Colors.grey.shade600)),
           ],
         ),
       ),
